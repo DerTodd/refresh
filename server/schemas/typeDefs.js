2acaea495f1772d69d58da9f288ff84aa6993a0d
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-12-15 16:07:26 -0600

    Updating typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,45 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+type User{
+    _id: ID
+    username:String!
+    firstname:String!
+    lastname:String!
+    title:String
+    email:String!
+    phones:[Array]
+    password:String!
+    bio:String
+    picture:String
+    date_joined:String
+}
 
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query{
+    me: User
+    users:[User]
+    user(userid: ID!):User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(
+        username:String!, 
+        firstname:String!, 
+        lastname:String!, 
+        title:String, 
+        email:String!,
+        phones:Array, 
+        password:String!,
+        bio:String,
+        picture:String): Auth
+    deleteUser(userId: ID!): User
+}
 
 `;
 module.exports = typeDefs;
